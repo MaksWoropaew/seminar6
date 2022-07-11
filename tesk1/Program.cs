9efd8e267955ffcc377120c272de605fc8dd3904
M.Voropaev <maks.woropaew@mail.ru>
2022-07-11 21:19:43 +0300

    add tesk

--- tesk1/Program.cs
+++ tesk1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Zadacha1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                string? str = Console.ReadLine();
+                int num = Convert.ToInt32(str);
+            }
+            catch
+            {
+
+                Console.WriteLine("Вы ввели не правильные данные");
+            }
+        }
+     }
+}
+
